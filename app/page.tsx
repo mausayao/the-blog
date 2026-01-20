1f9c2ed1a9a78027a8871bf11b51f83007b83430
mausayao <mauricio.sayao@gmail.com>
2026-01-20 19:30:21 -0400

    container component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
+import { Container } from "@/components/Container/Index";
 import { Posts } from "@/components/Posts";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function Home() {
     return (
-        <div>
+        <Container>
+            <header>
+                <h1>Aqui o Titulo</h1>
+            </header>
             <Suspense fallback={<SpinLoader />}>
                 <Posts />
             </Suspense>
-        </div>
+            <footer>
+                <h1>Aqui o footer</h1>
+            </footer>
+        </Container>
     );
 }
