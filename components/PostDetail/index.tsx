676261c158f7429aee5c93df47893e0fd4af7e14
mausayao <mauricio.sayao@gmail.com>
2026-01-30 20:54:10 -0400

    post content component

--- components/PostDetail/index.tsx
+++ components/PostDetail/index.tsx
@@ -1,5 +1,9 @@
 import { findBySlug } from "@/lib/post/queries";
 import { notFound } from "next/navigation";
+import { PostHeader } from "../PostHeader";
+import Image from "next/image";
+import { PostDate } from "../PostDate";
+import { PostContent } from "../PostContent";
 
 type PostDetailProps = {
     slug: string;
@@ -13,8 +17,28 @@ export async function PostDetail({ slug }: PostDetailProps) {
     }
 
     return (
-        <div>
-            <p>{post.title}</p>
-        </div>
+        <article>
+            <header className="flex flex-col mb-4 gap-4">
+                <PostHeader url={`/post/${post.slug}`} as="h1">
+                    {post.title}
+                </PostHeader>
+
+                <Image
+                    src={post.coverImageUrl}
+                    alt={post.title}
+                    width={1200}
+                    height={720}
+                    className="rounded-xl"
+                />
+
+                <p>
+                    {post.author} | <PostDate createdAt={post.createdAt} />
+                </p>
+            </header>
+
+            <p className="text-xl mb-4 text-slate-600">{post.excerpt}</p>
+
+            <PostContent content={post.content} />
+        </article>
     );
 }
