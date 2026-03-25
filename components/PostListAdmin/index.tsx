7848ed0f0afcd604ba1cfc75578f179bbf2d716a
mausayao <mauricio.sayao@gmail.com>
2026-03-24 20:42:26 -0400

    added admim button action

--- components/PostListAdmin/index.tsx
+++ components/PostListAdmin/index.tsx
@@ -2,6 +2,7 @@ import { findPostsAdmin } from "@/lib/post/queries/admin";
 import clsx from "clsx";
 import { Trash2Icon } from "lucide-react";
 import Link from "next/link";
+import { DeletePostButton } from "../Admin/DeletePostButton";
 
 export default async function PostListAdmin() {
     const posts = await findPostsAdmin();
@@ -26,17 +27,7 @@ export default async function PostListAdmin() {
                             </span>
                         )}
 
-                        <button
-                            className={clsx(
-                                "text-red-500 cursor-pointer transition",
-                                "[&_svg]:w-4 [&_svg]:h-4",
-                                "hover:scale-120 hover:text-red-700",
-                            )}
-                            aria-label={`Apagar post: ${post.title}`}
-                            title={`Apagar post: ${post.title}`}
-                        >
-                            <Trash2Icon />
-                        </button>
+                        <DeletePostButton id={post.id} title={post.title} />
                     </div>
                 );
             })}
